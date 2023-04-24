920283dfdce7effb4e9173e7c8123dcd3bd89dcb
Peter Ayeni <ayenicology@gmail.com>
2023-04-24 17:06:39 +0100

    Start Introduction

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
-# Nextra Docs Template 
-
-This is a template for creating documentation with [Nextra](https://nextra.site).
-
-[**Live Demo →**](https://nextra-docs-template.vercel.app)
-
-[![](.github/screenshot.png)](https://nextra-docs-template.vercel.app)
+# Tech Clarity Coach
 
 ## Quick Start
 
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Tech Clarity Coach</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/peterayeniofficial/techclaritycoach",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/peterayeniofficial/techclaritycoach",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Tech Clarity Coach",
   },
-}
+};
 
-export default config
+export default config;
