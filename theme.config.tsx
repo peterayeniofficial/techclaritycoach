a50b003e69d20949e668afa9c624dfecae7ae681
Peter Ayeni <ayenicology@gmail.com>
2023-04-28 13:28:13 +0100

    update discord

--- theme.config.tsx
+++ theme.config.tsx
@@ -7,7 +7,7 @@ const config: DocsThemeConfig = {
     link: "https://github.com/peterayeniofficial/techclaritycoach",
   },
   chat: {
-    link: "https://discord.com",
+    link: "https://discord.gg/mC4CvZC8",
   },
   docsRepositoryBase: "https://github.com/peterayeniofficial/techclaritycoach",
   useNextSeoProps() {
