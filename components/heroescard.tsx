33dd462cf4b8ca2c631bee4729939b97b969329b
Peter Ayeni <ayenicology@gmail.com>
2023-06-05 01:52:26 +0100

    fix build err

--- components/heroescard.tsx
+++ components/heroescard.tsx
@@ -1,4 +1,4 @@
-import { Card } from "nextra-theme-docs";
+import { Card, Cards } from "nextra-theme-docs";
 import Image from "next/image";
 
 const heroes = [
@@ -53,26 +53,30 @@ function Heroes() {
               <p className="my-0 text-xs dark:text-white mb-2">
                 {heroe.description}
               </p>
+
               <Card
                 icon={
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
-                    stroke-width="1.5"
+                    strokeWidth="1.5"
                     stroke="currentColor"
                     className="w-6 h-6"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
                     />
                   </svg>
                 }
                 title={heroe.handle}
                 href={heroe.link}
-              />
+                arrow
+              >
+                <></>
+              </Card>
             </div>
           </div>
         ))}
