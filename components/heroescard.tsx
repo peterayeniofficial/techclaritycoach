0bd708882e498c7a93d3af31c5f485669ae87fcf
Peter Ayeni <ayenicology@gmail.com>
2023-06-14 08:00:23 +0100

    improve heores card style

--- components/heroescard.tsx
+++ components/heroescard.tsx
@@ -1,58 +1,39 @@
-import { Card, Cards } from "nextra-theme-docs";
 import Image from "next/image";
 import { heroes } from "data/heroes";
+import Link from "next/link";
 
 function Heroes() {
   return (
     <>
       <div className="grid grid-cols-2 gap-6 my-12 sm:grid-cols-3 ">
         {heroes.map(({ ...heroe }, i) => (
-          <div
-            className="flex items-center space-x-4"
-            key={heroe.name.split(" ").join("-")}
-          >
-            <div className="rounded-lg overflow-hidden">
-              <Image
-                src={heroe.imageURL} // Path to your image
-                alt={heroe.name} // Alt text for your image
-                width={300} // Width of your image
-                height={300} // Height of your image
-                className="object-cover"
-              />
-            </div>
-            <div>
-              <div className="my-0 font-medium dark:text-white mb-3">
-                {heroe.name}
+          <Link href={heroe.link}>
+            <div
+              className="flex items-center space-x-4"
+              key={heroe.name.split(" ").join("-")}
+            >
+              <div className="rounded-lg overflow-hidden">
+                <Image
+                  src={heroe.imageURL} // Path to your image
+                  alt={heroe.name} // Alt text for your image
+                  width={300} // Width of your image
+                  height={300} // Height of your image
+                  className="object-cover"
+                />
               </div>
-              <p className="my-0 text-xs dark:text-white mb-2">
-                {heroe.description}
-              </p>
-
-              <Card
-                icon={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59"
-                    />
-                  </svg>
-                }
-                title={heroe.handle}
-                href={heroe.link}
-                arrow
-              >
-                <></>
-              </Card>
+              <div>
+                <div className="my-0 font-medium dark:text-white mb-3">
+                  {heroe.name}
+                </div>
+                <p className="my-0 text-xs dark:text-white mb-2">
+                  {heroe.description}
+                </p>
+                <div className="my-0 font-small dark:text-white mb-3">
+                  {heroe.handle}
+                </div>
+              </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </>
